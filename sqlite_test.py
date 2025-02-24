6d2e353c21a223fb740a90b31e60440942ce5b5d
RubyE21 <RubyEstrada@my.unt.edu>
2025-02-23 22:38:13 -0600

    Update sqlite_test.py
    Updated code that communicates site and the database

--- sqlite_test.py
+++ sqlite_test.py
@@ -1,3 +1,10 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import sqlite3
+
+app = Flask(__name__)
+CORS(app)
+
 import sqlite3
 
 conn = sqlite3.connect('test.db')
@@ -202,3 +209,42 @@ cursor.execute(create_table_users)
 cursor.execute(create_table_listings)
 conn.commit()
 conn.close()
+
+@app.route('/textbooks', methods=['POST'])
+def add_textbook():
+    data = request.get_json()
+    try:
+        conn = sqlite3.connect('test.db')
+        cursor = conn.cursor()
+        insert_sql = """
+        INSERT INTO tbl_textbooks(t_name, t_class, t_professor, t_ebook, t_required, t_link)
+        VALUES (?, ?, ?, ?, ?, ?)
+        """
+        textbook_data = (data['t_name'], data['t_class'], data['t_professor'], data['t_ebook'], data['t_required'], data['t_link'])
+        cursor.execute(insert_sql, textbook_data)
+        conn.commit()
+        return jsonify({'message': 'Textbook added successfully'}), 201
+    except sqlite3.Error as error:
+        return jsonify({'error': str(error)}), 400
+    finally:
+        if conn:
+            conn.close()
+
+@app.route('/search', methods=['GET'])
+def search_textbooks():
+    query = request.args.get('query')
+    try:
+        conn = sqlite3.connect('test.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM tbl_textbooks WHERE t_name LIKE ?", ('%' + query + '%',))
+        search_results = cursor.fetchall()
+        return jsonify(search_results)
+    except sqlite3.Error as error:
+        return jsonify({'error': str(error)}), 400
+    finally:
+        if conn:
+            conn.close()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+        
